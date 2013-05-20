723643d8a0f8469b5de4b05be6a1775bbb8a7e1c
Erik Michaels-Ober <sferik@gmail.com>
2013-05-20 15:36:48 -0700

    Append a rect for each data point

--- js/chart.js
+++ js/chart.js
@@ -4,12 +4,11 @@ var container = document.getElementById('container');
 button.onclick = function () {
   var svg = d3.select('div#container svg');
   var data = [100, 75, 150];
-  var rect1 = svg.append('rect');
-  var rect2 = svg.append('rect');
-  var rect3 = svg.append('rect');
 
   svg.selectAll('rect')
     .data(data)
+    .enter()
+    .append('rect')
     .attr("height", 30)
     .attr("width", 0)
     .transition()
