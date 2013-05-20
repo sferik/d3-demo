90e6c7af2eb2265c409395e31aa4cc0663f4c460
Erik Michaels-Ober <sferik@gmail.com>
2013-05-20 14:40:14 -0700

    Increase offset with each click

--- js/chart.js
+++ js/chart.js
@@ -1,10 +1,14 @@
 var button = document.getElementById('button');
 var container = document.getElementById('container');
 
+var xOffset = 0;
+
 button.onclick = function () {
+  xOffset += 20;
   var svg = d3.select('div#container svg');
   svg.append('rect')
     .attr({
+      "x": xOffset,
       "width": 100,
       "height": 100
     })
