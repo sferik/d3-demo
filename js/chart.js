044a43885748ffd1baa48471245d4ab32518108e
Erik Michaels-Ober <sferik@gmail.com>
2013-05-20 14:27:02 -0700

    Use method chaining

--- js/chart.js
+++ js/chart.js
@@ -3,8 +3,8 @@ var container = document.getElementById('container');
 
 button.onclick = function () {
   var svg = d3.select('div#container svg');
-  var rectangle = svg.append('rect');
-  rectangle.attr("width", 100);
-  rectangle.attr("height", 100);
-  rectangle.style("fill", "green");
+  svg.append('rect')
+    .attr("width", 100)
+    .attr("height", 100)
+    .style("fill", "green");
 };
